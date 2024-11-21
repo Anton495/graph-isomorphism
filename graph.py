953606a75fe3d2d39d895449eb21e1fb32e2366d
Anton Korneev <45163732+Anton495@users.noreply.github.com>
2024-11-21 16:44:37 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -42,7 +42,7 @@ class Graph:
         return graph2
         
     @staticmethod
-    def oneway_network(graph, start_vertex, end_vertex=None, N=None):
+    def oneway_network(graph, start_vertex, N=None, end_vertex=None):
     
         if N == None: 
             N = len(graph)-1
@@ -63,24 +63,24 @@ class Graph:
         return network
     
     @staticmethod
-    def twoway_network(graph, first_vertex, end_vertex):
+    def twoway_network(graph, first_vertex, end_vertex=None):
     
         n = len(graph) if end_vertex==None else len(graph)-1
         N = n//2 if n//2 == n/2 else n//2+1
     
-        virtual_network_1 = Graph.oneway_network(graph, first_vertex, end_vertex, N)
+        virtual_network_1 = Graph.oneway_network(graph, first_vertex, N, end_vertex)
     
         if N//2 == N/2:
             if end_vertex==None:        
                 network_2 = list(reversed(virtual_network_1))
             else:
-                network_2 = Graph.oneway_network(graph, end_vertex, first_vertex, N)
+                network_2 = Graph.oneway_network(graph, end_vertex, N, first_vertex)
                 network_2 = list(reversed(network_2))
         else:
             if end_vertex==None:  
                 network_2 = list(reversed(virtual_network_1[:-1]))
             else:
-                network_2 = Graph.oneway_network(graph, end_vertex, first_vertex, N-1)
+                network_2 = Graph.oneway_network(graph, end_vertex, N-1, first_vertex)
                 network_2 = list(reversed(network_2))
     
         virtual_network_2 = []
